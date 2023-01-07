9419da3b1f206db73ac1be668e16bc569ac31226
pat <pat@Jerry>
2023-01-07 01:10:50 +0100

    removed java nio impl

--- javaPFS/src/main/java/module-info.java
+++ javaPFS/src/main/java/module-info.java
@@ -2,29 +2,24 @@ import de.hechler.patrick.zeugs.pfs.FSProvider;
 import de.hechler.patrick.zeugs.pfs.impl.pfs.PatrFSProvider;
 
 module de.hechler.patrick.zeugs.pfs {
-
+	
 	exports de.hechler.patrick.zeugs.pfs;
 	exports de.hechler.patrick.zeugs.pfs.interfaces;
 	exports de.hechler.patrick.zeugs.pfs.misc;
 	exports de.hechler.patrick.zeugs.pfs.opts;
 	
-	// for now java package is only visible for testing
-	exports de.hechler.patrick.zeugs.pfs.java to de.hechler.patrick.zeugs.check;
-
 	exports de.hechler.patrick.zeugs.pfs.java.impl to de.hechler.patrick.zeugs.check;
 	exports de.hechler.patrick.zeugs.pfs.impl.pfs to de.hechler.patrick.zeugs.check;
-
+	
 	opens de.hechler.patrick.zeugs.pfs to de.hechler.patrick.zeugs.check;
 	opens de.hechler.patrick.zeugs.pfs.interfaces to de.hechler.patrick.zeugs.check;
 	opens de.hechler.patrick.zeugs.pfs.misc to de.hechler.patrick.zeugs.check;
 	opens de.hechler.patrick.zeugs.pfs.opts to de.hechler.patrick.zeugs.check;
-	opens de.hechler.patrick.zeugs.pfs.java to de.hechler.patrick.zeugs.check;
 	opens de.hechler.patrick.zeugs.pfs.java.impl to de.hechler.patrick.zeugs.check;
 	opens de.hechler.patrick.zeugs.pfs.impl.pfs to de.hechler.patrick.zeugs.check;
 	
 	uses FSProvider;
 	
-	// when the java package is finished, remember to add the provider
-	
 	provides FSProvider with PatrFSProvider;
+	
 }
