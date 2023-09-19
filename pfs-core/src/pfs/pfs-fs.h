ea2ae879b4bfcdeb96083ea8d6fb1053e8d2918d
pat <pat@Jerry>
2023-09-19 18:32:34 +0200

    update

---
+++ pfs-core/src/pfs/pfs-fs.h
@@ -0,0 +1,22 @@
+/*
+ * pfs-all.h
+ *
+ *  Created on: Sep 19, 2023
+ *      Author: pat
+ */
+
+#ifndef EXPORTS_INCLUDE_PFS_FS_H_
+#define EXPORTS_INCLUDE_PFS_FS_H_
+
+#include "pfs.h"
+#include "pfs-constants.h"
+#include "pfs-element.h"
+#include "pfs-err.h"
+#include "pfs-file.h"
+#include "pfs-folder.h"
+#include "pfs-iter.h"
+#include "pfs-mount.h"
+#include "pfs-pipe.h"
+#include "pfs-stream.h"
+
+#endif /* EXPORTS_INCLUDE_PFS_FS_H_ */
