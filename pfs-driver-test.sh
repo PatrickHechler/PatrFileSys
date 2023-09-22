87309e49710315f69e4a278b9c5816fdfa920ddc
pat <pat@Jerry>
2023-09-22 23:39:42 +0200

    update

---
+++ pfs-driver-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ./pfs-driver
+make all
+sudo dmesg --follow &
+echo 'add no patr-fs driver'
+sudo insmod patr_fs_driver.ko
+sleep 1
+echo 'remove no patr-fs driver'
+exec sudo rmmod patr_fs_driver.ko
