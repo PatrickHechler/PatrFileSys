bf444c9e64ea2e2f7e34ec3a6caa0f0f564f1910
pat <pat@Jerry>
2023-09-23 17:15:34 +0200

    update

--- pfs-driver/patr_fs_driver.c
+++ pfs-driver/patr_fs_driver.c
@@ -60,9 +60,9 @@ MODULE_ALIAS_FS("patrfs");
 
 static int __init patr_fs_init(void) {
 	int res = register_filesystem(&patr_fs_type);
-	printk(KERN_NOTICE "PatrFS: init val=%d\n", res);
-	if (!res) {
-		printk(KERN_ERR "PatrFS: could not register the file system\n");
+	printk(KERN_NOTICE "PatrFS: init val\n");
+	if (res) {
+		printk(KERN_ERR "PatrFS: could not register the file system: %d\n", res);
 	}
 	return res;
 }
