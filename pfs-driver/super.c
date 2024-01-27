9f4042fa834e667734ddfae0b6878ec780b8447f
PatrickHechler <gitHub@ph.anderemails.de>
2024-01-27 16:52:52 +0100

    change

--- pfs-driver/super.c
+++ pfs-driver/super.c
@@ -148,9 +148,8 @@ static int patr_fs_fill_super(struct super_block *sb, void *data, int silent) {
 		return -EIO;
 	}
 	printk(KERN_DEBUG MY_NAME ": fill super: call sb_getblk_gfp(sb=%p, 0U, 0)\n", sb);
-	struct buffer_head *bh = sb_getblk_gfp(sb, 0U, 0);
+	struct buffer_head *bh = sb_getblk(sb, 0U);
 	printk(KERN_DEBUG MY_NAME ": fill super: sb_getblk_gfp(sb=%p, 0U, 0) returned: %p : %u\n", sb, bh, IS_ERR(bh));
-	return -EINVAL;
 	if (IS_ERR(bh)) {
 		kfree(fsi);
 		return PTR_ERR(bh);
